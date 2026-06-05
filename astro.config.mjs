ff643d54c1bc232775bdcc3a8d18e09e154a47b0
Jonathan Ortega <ortegajonathanpedro@gmail.com>
2026-06-05 20:32:45 -0300

    feat: output to docs/ for Hostinger deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  site: 'https://www.laglera.com.ar'
+  site: 'https://www.laglera.com.ar',
+  outDir: './docs'
 });
